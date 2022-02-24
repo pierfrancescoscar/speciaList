425149b857d179007cc18038cb4740149102141d
pierfrancescoscar <scarinci401@gmail.com>
2022-02-24 16:59:08 +0100

    ADD: layouts folders

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = User::all();
+        return view('logged.home', compact('data'));
     }
 }
