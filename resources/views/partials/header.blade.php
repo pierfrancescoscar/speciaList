8bd100fe4ac7ba8b36bec6f101c6fb776b537a6f
stefanofilippini <filippini.stefano96@gmail.com>
2022-02-25 18:12:39 +0100

    ADD: header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,29 +1,27 @@
 {{-- Navbar --}}
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
+            {{-- logo --}}
+            <a class="navbar-brand" href="{{ url('/') }}"><img class="logo" src="{{ asset('assets/Specialist_Logo_Transparent_Tavola_disegno_1.png') }}" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
+                <ul class="navbar-nav ml-auto d-flex aligni-items-center">
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li> 
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('FAQ') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                        </li>
                         @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link btn-1 ms-2 p-2" href="{{ route('register') }}">{{ __('Sei uno specialista? Registrati!') }}</a>
                             </li>
                         @endif
                     @else
