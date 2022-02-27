6d6060837408c5fc72aa395aca2adff703ef0045
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-02-27 18:06:36 +0100

    add 1

--- resources/views/doctors/show.blade.php
+++ resources/views/doctors/show.blade.php
@@ -8,25 +8,35 @@
             <div class="left_container ">
 
                 {{-- Profile Pic --}}
+                <div class="title_container_info mb-5">
+                    <span>{{ $doctor->name }} {{ $doctor->surname }}</span>
+                </div>
+
                 @if ($doctor->profile_pic)
 
-                <div class="profile_pic">
+                <div class="profile_pic mb-2">
                     <img class="img-fluid" src="{{ asset('storage/' . $doctor->profile_pic) }}" alt="{{ $doctor->name }}">
                 </div>
 
                 @endif
 
-                <div class="doctor_primary_info text-center">
+                <div class="doctor_primary_info">
 
                     {{-- doctor id --}}
-                    <h2 class="mb-2">{{ $doctor->name }} {{ $doctor->surname }}</h2>
-
                     {{-- doctor category --}}
-                    <h3 class="mb-2">categoria</h3>
+                    <h4 class="mb-2 mt-2 text-center">Specializzazione:</h4>
+                    <ul class="">
+                        @foreach($doctor->categories as $category)
+                            <li class="">{{ $category->name }}</li>
+                        @endforeach
+                    </ul>
                 </div>
 
-                <a class="btn btn-primary" href="{{ route('admin.doctor.edit', $doctor->id) }}">Edit</a>
+                <a class="btn btn-edit" href="{{ route('admin.doctor.edit', $doctor->id) }}">Modifica il tuo profilo</a>
 
+                <div class="statistiche">
+
+                </div>
             </div>
 
             <div class="right_container">
@@ -71,7 +81,7 @@
                     {{-- Description --}}
                     <span class="name_info_row">Descrizione</span>
 
-                    <div class="row_info">
+                    <div class="row_info description">
                         <p class="mb-0">{{ $doctor->description }}</p>
                     </div>
 
@@ -81,14 +91,6 @@
                     <div class="row_info">
                         <span class="mb-0">{{ $doctor->address }}</span>
                     </div>
-
-                    {{-- Specializzazione --}}
-                    <div class="mt-5">
-                        <strong class="badge bg-success mt-3">Specializzazione:</strong>
-                        @foreach($doctor->categories as $category)
-                            {{ $category->name }},
-                        @endforeach
-                    </div>
                 </div>
             </div>
         </div>
@@ -97,8 +99,8 @@
         
 
         {{-- Action buttons  --}}
-        <a class="btn btn-warning" href="{{ route('admin.doctor.edit', $doctor->id) }}">Edit</a>
-        <a class="btn btn-danger" href="/">Back to archive</a>
+        {{-- <a class="btn btn-warning" href="{{ route('admin.doctor.edit', $doctor->id) }}">Edit</a>
+        <a class="btn btn-danger" href="/">Back to archive</a> --}}
     </div>
 
 
