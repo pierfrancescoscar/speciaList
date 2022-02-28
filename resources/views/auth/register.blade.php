b3293d7c508990b959416f5e985de17bcca1eb4a
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-02-28 15:10:25 +0100

    add 1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -92,7 +92,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <a href="{{ asset('doctors.create') }}">{{ __('Register') }}</a>
+                                    {{ __('Register') }}
                                     {{-- {{ __('Register') }} --}}
                                 </button>
                             </div>
