df7a97860b2145f784ca6b15bd6814baccacb643
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-02-28 16:34:13 +0100

    ADD: show

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -93,7 +93,6 @@
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
-                                    {{-- {{ __('Register') }} --}}
                                 </button>
                             </div>
                         </div>
