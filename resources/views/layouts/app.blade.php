5e498349f4cf43ee6461a5dbe04687c3126bb62b
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2022-03-03 18:13:42 +0100

    ADD doctor carousel (Vue components)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -50,6 +47,7 @@
     </div>
 
     {{-- Scripts --}}
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/category_validation.js') }}" defer></script>
 
 </body>
