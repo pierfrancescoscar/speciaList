479e9348cdfd17b2cfaea4c516f7e0d2b3c625a8
lnannettik <l.nannetti.k@gmail.com>
2022-03-04 10:37:59 +0100

    ADD: 2 pat

--- resources/views/guests/patacchi.blade.php
+++ resources/views/guests/patacchi.blade.php
@@ -1,13 +1,28 @@
 <section class="patacchi">
     <div class="patacco my-5">
-        <div class="bg_chiaro col-8 offset-2 col-lg-10 offset-lg-1 ">
+        <div class="bg_light col-10 offset-1">
             <div class="text_container col-12 col-lg-6 offset-lg-6">
                 <h2 class="mb-4 title" ><strong>Sei un dottore o uno specialista sanitario?<br>Entra nella nuova era digitale</strong></h2>
                 <h5 class="mb-4 text">Un unico strumento per gestire tutte le prestazioni, le visite e le attività mediche.<br>Da web e da mobile, 24 ore su 24.
                 </h5>
                 <button class="btn btn-primary btn-1 col-12 col-lg-4">Scopri di più</button>
             </div>
-            <div class="bg_scuro col-xs-12 col-lg-5">
+            <div class="bg_dark col-xs-12 col-lg-5">
+                <i class="fa-solid fa-briefcase-medical borsa_img"></i>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="patacco my-5">
+        <div class="bg_light_green col-10 offset-1">
+            <div class="text_container col-12 col-lg-6">
+                <h2 class="mb-4 title" ><strong>Sei un dottore o uno specialista sanitario?<br>Entra nella nuova era digitale</strong></h2>
+                <h5 class="mb-4 text">Un unico strumento per gestire tutte le prestazioni, le visite e le attività mediche.<br>Da web e da mobile, 24 ore su 24.
+                </h5>
+                <button class="btn btn-primary btn-2 col-12 col-lg-4">Scopri di più</button>
+            </div>
+            <div class="bg_dark_green col-xs-12 col-lg-5 offset-lg-6">
                 <i class="fa-solid fa-briefcase-medical borsa_img"></i>
             </div>
         </div>
