506d6b0c7ce1a20b18b4989fad073c8c67dc9bda
stefanofilippini <filippini.stefano96@gmail.com>
2022-03-13 15:39:35 +0100

    ADD: general style

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(SubscriptionsTableSeeder::class);
+        //$this->call(SubscriptionsTableSeeder::class);
 
     }
 }
